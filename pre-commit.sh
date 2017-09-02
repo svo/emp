e67a183dde809328dd5322722cc4c2caac922873
Sean Van Osselaer <svanosselaer@aconex.com>
2017-09-02 15:31:55 +1000

    Running unit and quality tests in pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 source bash/common.sh
+
+lein do clean, deps
+
+lein ancient
+
+lein test
+unit=$?
+echo_result "Unit Tests" $unit
+
+lein quality
+quality=$?
+echo_result "Quality Tests" $quality
