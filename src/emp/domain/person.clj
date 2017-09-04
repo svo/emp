5ef16b0f80ef7035e915177c4e7f74d75531c345
Sean Van Osselaer <svanosselaer@aconex.com>
2017-09-05 08:11:04 +1000

    Ensuring person first name is a string

--- src/emp/domain/person.clj
+++ src/emp/domain/person.clj
@@ -4,4 +4,5 @@
 
 (defn create
   [first_name last_name]
+  {:pre [(instance? java.lang.String first_name)]}
   (->Person first_name last_name))
--- test/emp/domain/person_test.clj
+++ test/emp/domain/person_test.clj
@@ -1,20 +1,24 @@
 (ns emp.domain.person-test
   (:require [emp.domain.person :as person :refer [->Person]])
-  (:use [midje.sweet :only [facts fact => anything]]))
+  (:use [midje.sweet :only [facts fact => anything throws]]))
 
 (facts
   "record"
 
   (fact
     "should have first name"
-    (:first_name (->Person ..first_name.. anything)) => ..first_name..)
+    (:first_name (->Person "..first_name.." anything)) => "..first_name..")
 
   (fact
     "should have last name"
-    (:last_name (->Person anything ..last_name..)) => ..last_name..))
+    (:last_name (->Person "anything" ..last_name..)) => ..last_name..))
 
 (fact
   "should get person"
-  (person/create ..first_name..
-                 ..last_name..) => (->Person ..first_name..
+  (person/create "..first_name.."
+                 ..last_name..) => (->Person "..first_name.."
                                              ..last_name..))
+
+(fact
+  "should error if first name is not a string"
+  (person/create ..first_name.. anything) => (throws AssertionError))
