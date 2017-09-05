426a52d51bd1477f299c3e028fe033299b3892de
Sean Van Osselaer <svanosselaer@aconex.com>
2017-09-05 17:01:58 +1000

    Ensuring employee person is a 'Person'

--- src/emp/domain/employee.clj
+++ src/emp/domain/employee.clj
@@ -1,7 +1,9 @@
-(ns emp.domain.employee)
+(ns emp.domain.employee
+  (:import [emp.domain.person Person]))
 
 (defrecord Employee [person])
 
 (defn create
   [person]
+  {:pre [(instance? Person person)]}
   (->Employee person))
--- test/emp/domain/employee_test.clj
+++ test/emp/domain/employee_test.clj
@@ -1,14 +1,21 @@
 (ns emp.domain.employee-test
-  (:require [emp.domain.employee :as employee :refer [->Employee]])
-  (:use [midje.sweet :only [facts fact =>]]))
+  (:require [emp.domain.employee :as employee :refer [->Employee]]
+            [emp.domain.person :refer [map->Person]])
+  (:use [midje.sweet :only [facts fact => throws]]))
 
 (facts
   "record"
 
   (fact
     "should have person"
-    (:person (->Employee ..person..)) => ..person..))
+    (let [person (map->Person {})]
+      (:person (->Employee person)) => person)))
 
 (fact
   "should get employee"
-  (employee/create ..person..) => (->Employee ..person..))
+  (let [person (map->Person {})]
+    (employee/create person) => (->Employee person)))
+
+(fact
+  "should error if person is not a Person"
+  (employee/create ..person..) => (throws AssertionError))
