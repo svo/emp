f8aa398fc0e9fe39d94ee08ab27b119d7a02b41e
Sean Van Osselaer <svanosselaer@aconex.com>
2017-10-06 06:10:31 +1100

    Extracting currency symbol as constant in PDF generator

--- src/emp/application/pdf_generator.clj
+++ src/emp/application/pdf_generator.clj
@@ -1,9 +1,12 @@
 (ns emp.application.pdf-generator
   (:use [clj-pdf.core :only [pdf]]))
 
+(def ^:const CURRENCY_SYMBOL "$")
+(def ^:const PATH "/var/lib/emp/")
+
 (defn path
   [identifier]
-  (str "/var/lib/emp/" identifier ".pdf"))
+  (str PATH identifier ".pdf"))
 
 (defn generate
   [payslip]
@@ -22,8 +25,16 @@
                       (.payment-end-day payslip)
                       " of "
                       (.payment-year payslip))]
-     [:paragraph (str "Gross Income: $" (.gross-income payslip))]
-     [:paragraph (str "Income Tax: $" (.income-tax payslip))]
-     [:paragraph (str "Net Income: $" (.net-income payslip))]
-     [:paragraph (str "Super: $" (.super payslip))] ]
+     [:paragraph (str "Gross Income: "
+                      CURRENCY_SYMBOL
+                      (.gross-income payslip))]
+     [:paragraph (str "Income Tax: "
+                      CURRENCY_SYMBOL
+                      (.income-tax payslip))]
+     [:paragraph (str "Net Income: "
+                      CURRENCY_SYMBOL
+                      (.net-income payslip))]
+     [:paragraph (str "Super: "
+                      CURRENCY_SYMBOL
+                      (.super payslip))] ]
     (path (.identifier payslip))))
